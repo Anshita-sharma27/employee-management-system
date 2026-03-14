e8e127ed2264923e82bc37e8006de7cacb47eeb2
Anshita Sharma <sharmaanshita58a@gmail.com>
2026-03-14 23:10:36 +0530

    app

--- app.py
+++ app.py
@@ -106,5 +106,6 @@ def delete_employee(id):
     db.session.commit()
     return redirect(url_for('dashboard'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
